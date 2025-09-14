d720a07001702aa0870cac5d1b207a60dbe17da2
b3nz0 <pierre@buckleigh.net>
2025-09-14 19:03:27 +0100

    Update rf2serversetup.ps1

--- rf2serversetup.ps1
+++ rf2serversetup.ps1
@@ -12,6 +12,29 @@ cd .\steamcmd
 ./steamcmd +login anonymous +force_install_dir ..\rfactor2-dedicated +app_update 400300 +quit
 ./steamcmd +login anonymous +workshop_download_item 365960 951986312 +quit
 ./steamcmd +login anonymous +workshop_download_item 365960 1782911839 +quit
+./steamcmd +login anonymous +workshop_download_item 365960 2736021728 +quit
+./steamcmd +login anonymous +workshop_download_item 365960 1961493725 +quit
+./steamcmd +login anonymous +workshop_download_item 365960 2736075175 +quit
+./steamcmd +login anonymous +workshop_download_item 365960 2111766389 +quit
+./steamcmd +login anonymous +workshop_download_item 365960 1628206724 +quit
+./steamcmd +login anonymous +workshop_download_item 365960 1629100532 +quit
+./steamcmd +login anonymous +workshop_download_item 365960 1629067802 +quit
+./steamcmd +login anonymous +workshop_download_item 365960 1629142012 +quit
+./steamcmd +login anonymous +workshop_download_item 365960 1629177826 +quit
+./steamcmd +login anonymous +workshop_download_item 365960 1629165420 +quit
+./steamcmd +login anonymous +workshop_download_item 365960 2736019095 +quit
+./steamcmd +login anonymous +workshop_download_item 365960 2098773157 +quit
+./steamcmd +login anonymous +workshop_download_item 365960 2736009471 +quit
+./steamcmd +login anonymous +workshop_download_item 365960 2069376958 +quit
+./steamcmd +login anonymous +workshop_download_item 365960 2736015837 +quit
+./steamcmd +login anonymous +workshop_download_item 365960 2736012574 +quit
+./steamcmd +login anonymous +workshop_download_item 365960 788866138 +quit
+./steamcmd +login anonymous +workshop_download_item 365960 587034279 +quit
+./steamcmd +login anonymous +workshop_download_item 365960 2206725222 +quit
+./steamcmd +login anonymous +workshop_download_item 365960 2857238855 +quit
+./steamcmd +login anonymous +workshop_download_item 365960 951986312 +quit
+./steamcmd +login anonymous +workshop_download_item 365960 3003499664 +quit
+./steamcmd +login anonymous +workshop_download_item 365960 1885152462 +quit
 cp .\steamapps\workshop\content\365960\*\*.rfcmp $workDir\rfactor2-dedicated\Packages\
 
 # copy executables into root
@@ -25,4 +48,5 @@ New-NetFirewallRule -Program "C:\rfactor2-dedicated\rFactor2 Dedicated.exe" -Act
 New-NetFirewallRule -Program "C:\rfactor2-dedicated\rFactor2 Dedicated.exe" -Action Allow -Profile Public,Private -Direction Outbound -DisplayName "rFactor2 Server" -Description "Allow rFactor2 Server outbound connections"
 
 Write-Output "Success! rFactor2 dedicated server is available in " + $workDir + "rfactor2-dedicated\"
+
 Start-Sleep -s 3
